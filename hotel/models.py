8d8969ae8d3e5222c807ef24b1c884ebff88c2ea
Zharkynai <zharkynai.toichubaeva@gmail.com>
2022-02-18 15:45:32 +0600

    добавлеине модельки booking

--- hotel/models.py
+++ hotel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Room(models.Model):
@@ -15,4 +16,16 @@ class Room(models.Model):
 
     def __str__(self):
         return f'{self.room_num}. {self.category} with {self.beds} beds for {self.capacity} people'
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in_date = models.DateTimeField()
+    check_out_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user} has booked {self.room} from {self.check_in_date} to {self.check_out_date}'
+
 
